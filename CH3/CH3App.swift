f20eb9f19c9b75c84abce05fad20bb83f5525d42
giulia892 <giloffredo02@gmail.com>
2024-12-10 10:23:00 +0100

    Initial Commit

---
+++ CH3/CH3App.swift
@@ -0,0 +1,17 @@
+//
+//  CH3App.swift
+//  CH3
+//
+//  Created by Giulia Loffredo on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CH3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
